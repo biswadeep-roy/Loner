49970426ad734bbf6333f8ccf4fbcd0cf0a72c58
Biswadeep Roy <biswadeeproy1230@gmail.com>
2023-12-23 10:38:57 +0530

    another committt

--- reagister.js
+++ reagister.js
@@ -482,3 +482,487 @@ const reagister = () => {
 }
 
 export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
