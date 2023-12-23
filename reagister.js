b68b7b376b0692bfd9aafdb1adce2ab1a0a0e71d
Biswadeep Roy <biswadeeproy1230@gmail.com>
2023-12-23 10:30:58 +0530

    another committt

--- reagister.js
+++ reagister.js
@@ -9,3 +9,14 @@ const reagister = () => {
 }
 
 export default reagister
+import React from 'react'
+
+const reagister = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default reagister
