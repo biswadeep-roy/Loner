7e5d85c01a9ca3028ffc64e5358313821b163c53
Biswadeep Roy <biswadeeproy1230@gmail.com>
2023-12-23 10:35:07 +0530

    another committt

--- login.js
+++ login.js
@@ -174,3 +174,47 @@ const login = () => {
 }
 
 export default login
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
